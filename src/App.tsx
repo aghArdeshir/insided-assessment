06beb68df45ea9a1a8523597dce34217381962d1
aghArdeshir <ardeshireo@gmail.com>
2022-08-25 21:14:36 +0400

    Add an ugly view for now to make it beautiful later

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,36 @@
 import React from "react";
 import { AccessTokenInputModal } from "./AccessTokenInputModal";
+import { ERRORS } from "./constants/errors";
 import { useAccessToken } from "./hooks/useAccessToken";
+import { useCommits } from "./hooks/useCommits";
 import { ButtonWithKeyIcon } from "./RemoveMyAccessKeyButton";
 
 function App() {
   const { accessToken, setAccessToken, removeAccessToken } = useAccessToken();
+  const { commits, error } = useCommits(accessToken);
 
   if (!accessToken) {
     return <AccessTokenInputModal onSubmit={setAccessToken} />;
+  } else if (error === ERRORS.INVALID_TOKEN) {
+    return (
+      <div>
+        <h1>
+          Invalid GitHub Access Token key. Click&nbsp;
+          <button onClick={removeAccessToken}>Here</button>&nbsp;to remove your
+          access token and refresh
+        </h1>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        {commits.map((commit) => {
+          return <h1 key={commit.sha}>Commit: {commit.sha}</h1>;
+        })}
+        <ButtonWithKeyIcon onClick={removeAccessToken} />
+      </div>
+    );
   }
-
-  return (
-    <div>
-      <ButtonWithKeyIcon onClick={removeAccessToken} />
-    </div>
-  );
 }
 
 export default App;
