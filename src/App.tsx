8f0ae7315cd1f591fcbdebce446bfbcc4f3d7f33
aghArdeshir <ardeshireo@gmail.com>
2022-08-25 20:30:14 +0400

    setup App to work with accessTokenService

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { AccessTokenInputModal } from "./AccessTokenInputModal";
+import { accessTokenServiec } from "./services/accessTokenService";
 
 function App() {
+  const [accessToken, setAccessToken] = useState<string | null>(
+    accessTokenServiec.getAccessToken()
+  );
+
+  useEffect(() => {
+    if (accessToken) {
+      accessTokenServiec.setAccessToken(accessToken);
+    }
+  }, [accessToken]);
+
+  if (!accessToken) {
+    return <AccessTokenInputModal onSubmit={setAccessToken} />;
+  }
+
   return <div />;
 }
 
