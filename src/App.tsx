fd6b5e662202f3d206aa406087eac3809937cc5d
aghArdeshir <ardeshireo@gmail.com>
2022-08-26 10:25:09 +0400

    Add another if branch 🤦🏼‍♂️ for error handling

--- src/App.tsx
+++ src/App.tsx
@@ -14,13 +14,26 @@ function App() {
     return <AccessTokenInputModal onSubmit={setAccessToken} />;
   } else if (error === ERRORS.INVALID_TOKEN) {
     return (
-      <div>
-        <h1>
-          Invalid GitHub Access Token key. Click&nbsp;
-          <button onClick={removeAccessToken}>Here</button>&nbsp;to remove your
-          access token and refresh
-        </h1>
-      </div>
+      <h1>
+        Invalid GitHub Access Token key. Click&nbsp;
+        <button style={{ fontSize: "inherit" }} onClick={removeAccessToken}>
+          Here
+        </button>
+        &nbsp;to remove your access token and refresh
+      </h1>
+    );
+  } else if (error) {
+    return (
+      <h1>
+        Unknown error occurred! Click&nbsp;
+        <button
+          style={{ fontSize: "inherit" }}
+          onClick={window.location.reload}
+        >
+          Here
+        </button>
+        &nbsp;to reload the page
+      </h1>
     );
   } else {
     return (
