2fb16ddadc54bafd3f1a4ccd0602a6d0c1b798b9
aghArdeshir <ardeshireo@gmail.com>
2022-08-26 14:39:35 +0400

    make commits time counting live + add TODOs in Readme

--- src/hooks/useCommits.ts
+++ src/hooks/useCommits.ts
@@ -2,6 +2,8 @@ import { useCallback, useEffect, useState } from "react";
 import { fetchCommits } from "../services/fetchCmomits";
 import { T_Commit } from "../types";
 
+const ONE_SECOND = 1000;
+
 export function useCommits(accessToken: string | null) {
   const [error, setError] = useState<any>();
   const [commits, setCommits] = useState<T_Commit[]>([]);
@@ -32,6 +34,30 @@ export function useCommits(accessToken: string | null) {
       _fetchCommits();
     }
   }, [_fetchCommits, accessToken]);
+
+  useEffect(() => {
+    const intervalRef = setInterval(() => {
+      setCommits((commits) =>
+        commits.map((commit) => ({
+          ...commit,
+          commit: {
+            ...commit.commit,
+            author: {
+              ...commit.commit.author,
+              date: new Date(
+                new Date(commit.commit.author?.date || "").getTime() -
+                  ONE_SECOND
+              ),
+            },
+          },
+        }))
+      );
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalRef);
+    };
+  }, []);
 
   return { commits, error, refresh: _fetchCommits, loading };
 }
