1f637eea73afdd428568eee1d4148aabc4a924ea
aghArdeshir <ardeshireo@gmail.com>
2022-08-26 22:14:38 +0400

    fix the problem with window.location.reload invalid invocation

--- src/UnknownError.tsx
+++ src/UnknownError.tsx
@@ -2,7 +2,10 @@ export function UnknownError() {
   return (
     <h1>
       Unknown error occurred! Click&nbsp;
-      <button style={{ fontSize: "inherit" }} onClick={window.location.reload}>
+      <button
+        style={{ fontSize: "inherit" }}
+        onClick={() => window.location.reload()}
+      >
         Here
       </button>
       &nbsp;to reload the page
